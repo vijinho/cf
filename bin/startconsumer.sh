10a97559976b70bd66e6b2a3a63c23bdb3220a08
Vijay Mahrra <vijay.mahrra@gmail.com>
2015-04-27 02:48:09 +0100

    can load in generator now

--- bin/startconsumer.sh
+++ bin/startconsumer.sh
@@ -1,2 +1,1 @@
-cd consumer
-gunicorn --timeout 15 --graceful-timeout 10 --max-requests 16384 --max-requests-jitter 4096 --limit-request-line 256 --limit-request-fields 32 --limit-request-field_size 1024 consume:app
+gunicorn --pythonpath consumer --timeout 15 --graceful-timeout 10 --max-requests 16384 --max-requests-jitter 4096 --limit-request-line 256 --limit-request-fields 32 --limit-request-field_size 1024 consume:app
