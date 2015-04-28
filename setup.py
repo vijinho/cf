07f8f2d3f0377b5dd44619c4ef6c23e339b72e96
Vijay Mahrra <vijay.mahrra@gmail.com>
2015-04-28 02:16:18 +0100

    add cli

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ config = {
     'download_url': 'https://github.com/vijinho/cf',
     'version': '1.0',
     'keywords': ['rethinkdb','falcon','celery','rabbitmq','gunicorn','currency','cf'],
-    'install_requires': ['nose','future', 'click', 'rethinkdb', 'cython', 'falcon', 'gunicorn', 'librabbitmq', 'celery'],
+    'install_requires': ['nose','future', 'click', 'rethinkdb', 'reql-cli', 'cython', 'falcon', 'gunicorn', 'librabbitmq', 'celery'],
     'packages': ['cf'],
     'scripts': [],
     'name': 'cf'
