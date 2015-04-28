a63b3f3bebb1c7711021f6c4ab328d39355e818f
Vijay Mahrra <vijay.mahrra@gmail.com>
2015-04-28 19:47:56 +0100

    update scripts for test server

--- bin/delete-trade.sh
+++ bin/delete-trade.sh
@@ -1,1 +1,1 @@
-curl -i --user 'cf!':'CF:$2015' -H 'Content-Type: application/json' -H 'Accept: application/json' -X DELETE http://localhost:80/trade?id=$1
+curl -i --user 'cf!':'CF:$2015' -H 'Content-Type: application/json' -H 'Accept: application/json' -X DELETE http://bikr.es/trade?id=$1
--- bin/send-random-trade-direct.sh
+++ bin/send-random-trade-direct.sh
@@ -1,2 +1,2 @@
-curl -i --user 'cf!':'CF:$2015' -H 'Content-Type: application/json' -H 'Accept: application/json' -X POST -d "$(python cf/generate.py -l -q $1)" http://localhost:8000/trade
+curl -i --user 'cf!':'CF:$2015' -H 'Content-Type: application/json' -H 'Accept: application/json' -X POST -d "$(python cf/generate.py -l -q $1)" http://bikr.es/trade
 
